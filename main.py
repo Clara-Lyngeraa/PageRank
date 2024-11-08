257ed35cbc2c637d67e9ac72e0aa205708427cfe
Monica Hardt <monicahardt@Macbook-Pro-Monica.local>
2024-11-08 08:30:13 +0100

    idk

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import sys
 import networkx as nx
 import numpy as np
@@ -7,55 +8,52 @@ def page_rank(G: nx.DiGraph):
     n = G.number_of_nodes()
     rev_G: nx.DiGraph = G.reverse()
     branching = [G.out_degree(i) for i in G.nodes] #Outgoing edges from node i
-    dangling_nodes = [node for node in G.nodes if G.out_degree(node) == 0] #List of nodes with no outgoing edges
+    dangling_nodes = [int(node) for node in G.nodes if G.out_degree(node) == 0] #List of nodes with no outgoing edges
     x_0 = [1/n for i in range(n)] #Initial probability distribution // initial guess for ?? Take and multiplyby n
     m = 0.15
     S = np.full((n, n), 1/n)
+    print("n -------------")
     print(n)
-    print("-------------")
+    print("g------------")
+    print(G)
+    print("rev_g------------")
     print(rev_G)
-    print("-------------")
+    print("nodes")
+    print(G.nodes)
+    print("brancing ------------")
     print(branching)
-    print("-------------")
+    print("dangling_nodes ------------")
     print(dangling_nodes)
-    print("-------------")
+    print("x_0------------")
     print(x_0)
-    print("-------------")
+    print("m ------------")
     print(m)
 
     x_k = x_0
     Sx_k = np.dot(S, x_k)  #should be 1/n in all entries of the column vector S using the lemma that all entries in x_k sums to 1.
     print("Sx_k")
     print(Sx_k)
-    for k in range(50): #outer loop. Multiply current vector by M until eigenvector is reached
+    for k in range(3): #outer loop. Multiply current vector by M until eigenvector is reached
         dangling_sum = sum(x_k[j] / n for j in dangling_nodes) #sum of the pagerank score for each of the dangling nodes divided by the total number of nodes
         for i in range(n): #loop over all nodes
-            # Initialize an array to hold the contribution from A
             #get links to i
-            links_to_i = rev_G.successors(i) #using links to i, since A is sparse aka all other entries of A is 0. So matrix multiplication is too performance expensive
-            ak_sum = sum(1/n * x_k[j] for j in links_to_i)
-            x_k_one = (1-m)*ak_sum+(1-m)*dangling_sum+m*Sx_k
-            x_k = x_k_one
+            links_to_i = list(rev_G.successors(str(i))) #using links to i, since A is sparse aka all other entries of A is 0. So matrix multiplication is too performance expensive
+            ak_sum = sum(1/n * x_k[int(j)] for j in links_to_i)
+            M = (1-m)*ak_sum+(1-m)*dangling_sum+m*Sx_k
+            x_k_1 = [score * M for score in x_k]
+            print("***** PAGERANK******")
+            print(x_k_1)
+            x_k = x_k_1
+        
 
-def random_surfer(graph):
-    m = 0.15
-    return 0
-    
+
 
 def main():
     fh=open(sys.argv[1],'rb')
     G: nx.DiGraph=nx.read_adjlist(fh,create_using=nx.DiGraph()) #G the graph
     fh.close()
+    page_rank(G)
 
 
 if __name__ == "__main__":
     main()   
-
-        
-
-
-
-    
-
-    
-
