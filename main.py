81b0c40196236128b20a8406be19ef9d8ca45055
Monica Hardt <monicahardt@Macbook-Pro-Monica.local>
2024-11-10 16:09:57 +0100

    might be done

--- main.py
+++ main.py
@@ -5,47 +5,67 @@ import numpy as np
 
 #Vector x_k: entries corresponding to the pagerank score for each node in the graph at iteration k
 def page_rank(G: nx.DiGraph):
+
+    #number of nodes in the graph
     n = G.number_of_nodes()
+
+    #the reverse graph to use
     rev_G: nx.DiGraph = G.reverse()
-    branching = [G.out_degree(i) for i in G.nodes] #Outgoing edges from node i
-    dangling_nodes = [int(node) for node in G.nodes if G.out_degree(node) == 0] #List of nodes with no outgoing edges
-    x_0 = [1/n for i in range(n)] #Initial probability distribution // initial guess for ?? Take and multiplyby n
+
+    #Outgoing edges from node i
+    branching = np.array([G.out_degree(i) for i in G.nodes])
+
+    #Nodes with no outgoing edges
+    dangling_nodes = np.array([int(node) for node in G.nodes if G.out_degree(node) == 0])
+
+    #Initial probability distribution. The first vector
+    x_k = np.full(n, 1/n)
+
     m = 0.15
-    S = np.full((n, n), 1/n)
-    print("n -------------")
-    print(n)
-    print("g------------")
-    print(G)
-    print("rev_g------------")
-    print(rev_G)
-    print("nodes")
-    print(G.nodes)
-    print("brancing ------------")
-    print(branching)
-    print("dangling_nodes ------------")
-    print(dangling_nodes)
-    print("x_0------------")
-    print(x_0)
-    print("m ------------")
-    print(m)
 
-    x_k = x_0
-    Sx_k = np.dot(S, x_k)  #should be 1/n in all entries of the column vector S using the lemma that all entries in x_k sums to 1.
-    print("Sx_k")
-    print(Sx_k)
-    for k in range(3): #outer loop. Multiply current vector by M until eigenvector is reached
-        dangling_sum = sum(x_k[j] / n for j in dangling_nodes) #sum of the pagerank score for each of the dangling nodes divided by the total number of nodes
-        for i in range(n): #loop over all nodes
-            #get links to i
-            links_to_i = list(rev_G.successors(str(i))) #using links to i, since A is sparse aka all other entries of A is 0. So matrix multiplication is too performance expensive
-            ak_sum = sum(1/n * x_k[int(j)] for j in links_to_i)
-            M = (1-m)*ak_sum+(1-m)*dangling_sum+m*Sx_k
-            x_k_1 = [score * M for score in x_k]
-            print("***** PAGERANK******")
-            print(x_k_1)
-            x_k = x_k_1
+    # print("n -------------")
+    # print(n)
+    # print("g------------")
+    # print(G)
+    # print("rev_g------------")
+    # print(rev_G)
+    # print("nodes")
+    # print(G.nodes)
+    # print("brancing ------------")
+    # print(branching)
+    # print("dangling_nodes ------------")
+    # print(dangling_nodes)
+    # print("x_k------------")
+    # print(x_k)
+    # print("m ------------")
+    # print(m)
+
+     #outer loop. Multiply current vector by M until eigenvector is reached
+    for _ in range(50):
+
+        #sum of the pagerank score for each of the dangling nodes divided by the total number of nodes
+        dangling_sum = sum(x_k[j] / n for j in dangling_nodes) #maybe: dangling_sum = sum(x_k[j] for j in dangling_nodes) / n
         
+        #new varibale to hold new pagerank calculation
+        x_k_1 = np.zeros(n)
 
+        #loop over all nodes
+        for i in range(n):
+            #using links to i, since A is sparse aka all other entries of A is 0. 
+            # #So matrix multiplication is too performance expensive
+            links_to_node_i = list(rev_G.successors(str(i))) 
+
+            #what is happening here?
+            link_sum = sum(x_k[int(j)] / branching[int(j)] for j in links_to_node_i if branching[int(j)] > 0)
+
+            #update i'th entry in pagerank vector 
+            x_k_1[i] = (1 - m) * (link_sum + dangling_sum) + m * (1 / n) #1/n from S matrix
+
+        #update pagerank to new pagerank vector
+        x_k = x_k_1
+       
+    print("pagerank calculated to be:")
+    print(x_k)
 
 
 def main():
